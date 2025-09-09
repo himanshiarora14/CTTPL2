dc005372d87a4bca5dfe402cf08429dbe63542a4
Rrahullkumar <rahulkr811844@gmail.com>
2025-09-09 21:35:22 +0530

    added Ttile section

--- frontend/src/pages/AboutPage.jsx
+++ frontend/src/pages/AboutPage.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import CompanyOverView from '../components/CompanyOverView'
+import TeamsSection from '../components/Team'
+import Getintouch from '../components/Getintouch'
 
 const AboutPage = () => {
   return (
     <div>
       <CompanyOverView/>
+      <TeamsSection/>
+      <Getintouch/>
     </div>
   )
 }
