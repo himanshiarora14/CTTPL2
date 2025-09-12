e1824d00d1cc26c7b5c20616d806dabd5518f976
Rrahullkumar <rahulkr811844@gmail.com>
2025-09-12 20:56:20 +0530

    saftey and tech minor changes

--- frontend/src/pages/SafetyAndTech.jsx
+++ frontend/src/pages/SafetyAndTech.jsx
@@ -1,17 +1,17 @@
 import React from 'react'
-import Landing from '../components/saftey/landing'
-import SafetyMeasures from '../components/saftey/SafetyMeasures'
-import TechAndAi from '../components/saftey/TechAndAi'
+import Landing from '../Components/saftey/landing'
+import SafetyMeasures from '../Components/saftey/SafetyMeasures'
+import TechAndAi from '../Components/saftey/TechAndAi'
 // import Choice from '../components/Bestchoice'
-import Choice from '../components/Bestchoice'
+import Choice from '../Components/Bestchoice'
 // import Fleets from '../components/Fleets'
-import Fleets from '../components/Fleets'
+import Fleets from '../Components/Fleets'
 // import OurPresence from '../components/OurPresence'
 import OurPresence from '../components/OurPresence'
 
 // import ContactSection from '../components/Getintouch'
-import ContactSection from '../components/Getintouch'
-import Footer from '../components/Footer'
+import ContactSection from '../Components/Getintouch'
+import Footer from '../Components/Footer'
 
 
 const SafteyAndTech = () => {
