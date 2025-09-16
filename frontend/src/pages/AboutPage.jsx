d9225c2e37d1601d22130de20344432be4f965a6
riddhi16Khandelwal <khandelwalriddhi3@gmail.com>
2025-09-16 10:51:04 +0530

    add

--- frontend/src/pages/AboutPage.jsx
+++ frontend/src/pages/AboutPage.jsx
@@ -1,18 +1,39 @@
-import React from 'react'
-import ContactSection from '../components/Getintouch'
-import CompanyOverView from '../components/CompanyOverView'
-import TeamsSection from '../components/Team'
-import Getintouch from '../components/Getintouch'
-
+import React from 'react';
+// import CompanyOverView from '../Components/CompanyOverView';
+// import TeamsSection from '../Components/Team';
+import Getintouch from '../Components/Getintouch';
+// import Footer1 from '../Components/Aboutus/Footer1';
+import Ournation from '../Components/Aboutus/Ournation';
+import Ourservices from '../Components/Ourservices';
+import Bestchoice from '../Components/Aboutus/Bestchoice';
+import Fleets from '../Components/Fleets';
+import Leadership from '../Components/Aboutus/Leadership';
+import Mission from '../Components/Aboutus/Mission';
+import Box from '../Components/Aboutus/Box';
+import Inception from '../Components/Aboutus/Inception';
+import Page1 from '../Components/Aboutus/Page1';
 const AboutPage = () => {
   return (
     <div>
-      <CompanyOverView/>
-      <TeamsSection/>
-      <Getintouch/>
-      <ContactSection/>
+      {/* <CompanyOverView />
+      <TeamsSection /> */}
+      <Page1/>
+      <Inception/>
+     <Box/>
+      <Mission/>
+      <Ourservices />
+       <Leadership/>
+        <Bestchoice/>
+        <Fleets/>
+      <Ournation />
+       <Getintouch />
+       {/* <Footer1/> */}
+      
+      
+      
     </div>
-  )
-}
+  );
+};
 
-export default AboutPage
+export default AboutPage;
+
