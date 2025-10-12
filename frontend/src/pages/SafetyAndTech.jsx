b0925eecda7e545cd8a67d68e0875b2f99fcd173
Aryesh02 <aryeshsrivastava@gmail.com>
2025-10-13 00:17:37 +0530

    Frontend OurPresence File Changes made

--- frontend/src/pages/SafetyAndTech.jsx
+++ frontend/src/pages/SafetyAndTech.jsx
@@ -5,7 +5,8 @@ import TechAndAi from '../Components/saftey/TechAndAi'
 import Choice from '../Components/Bestchoice'
 import Fleets from '../Components/Fleets'
 // import OurPresence from '../components/OurPresence'
-import OurPresence from '../Components/OurPresence'
+// import OurPresence from '../Components/OurPresence'
+import OurPresence from "../Components/OurPresence"
 
 // import ContactSection from '../components/Getintouch'
 import ContactSection from '../Components/Getintouch'
