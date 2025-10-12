c14c887cf60a7c9ab657b1c8c610a0a55107ca9e
Aryesh02 <aryeshsrivastava@gmail.com>
2025-10-13 00:01:44 +0530

    Frontend Routing Changes made

--- frontend/src/Components/Booking.jsx
+++ frontend/src/Components/Booking.jsx
@@ -34,7 +34,7 @@ const OnSpotRentalsForm = () => {
     setLoading(true);
 
     try {
-      const response = await fetch("http://localhost:8080/sendemail", {
+      const response = await fetch("https://cttpl.onrender.com/sendemail", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(formData),
