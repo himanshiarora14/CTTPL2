7d3b5657feb5d0939e41291f30b1e5fc17537759
Aryesh02 <aryeshsrivastava@gmail.com>
2025-10-13 02:20:35 +0530

    Some naming changes made

--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -1,6 +1,6 @@
 // import Contact from "./Button/Contact"
 // import Contact2 from './Button/Contact2.jsx'
-import Contact from './Button/Contact.jsx'
+import Contact2 from './Button/Contact.jsx'
 import Embedded from '../Components/Button/Embedded'
 
 const Hero = () => {
@@ -34,7 +34,7 @@ const Hero = () => {
                                 Experience comfort, reliability, and tech-enabled journeys with India's leading travel partner—trusted by travellers and businesses alike.
                             </p>
 
-                            <Contact />
+                            <Contact2 />
 
                         </div>
                     </div>
